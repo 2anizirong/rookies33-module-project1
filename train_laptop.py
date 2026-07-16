829ff03da6506e6178135cf8bbc7dafe4094abbe
kawwwuy <kawwwuy@gmail.com>
2026-07-16 11:39:00 +0900

    model: 저장 파일명 변경/ 성능지표 출력 라벨 변경

--- train_laptop.py
+++ train_laptop.py
@@ -19,52 +19,40 @@ import joblib
 
 
 # %%
+# 2. 데이터셋 로드
 df = pd.read_csv('data/processed/0715ebay_laptops_model_ready_v1.csv')
-print(df.shape)
-print(df.columns.tolist())
-print(df.dtypes)
-df.head()
 
 # %%
 X = df.drop(columns=["price_usd"])
 y = df["price_usd"]
 
-print(X.shape, y.shape)
 
 # %%
-# 훈련데이터와 학습데이터 분리(8:2)
+# 3. 데이터 분리(8:2)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-print(X_train.shape, X_test.shape)
 
 # %%
-# 수치형 사양 열의 -1은 XGBoost가 자동으로 처리하도록 NaN으로 바꿈
-num_cols = [
-    'release_year', 'processor_speed_ghz', 'ram_gb', 'screen_size_inch', 
-    'resolution_width', 'resolution_height', 'ssd_gb', 'hdd_gb', 
-    'storage_capacity_gb', 'has_dual_storage', 'has_touchscreen', 
-    'has_backlit_keyboard', 'has_bluetooth', 'has_webcam', 'has_wifi'
-]
+# 3. 결측치 처리
+num_cols = X.select_dtypes(include="number").columns.tolist()
+
 X_train[num_cols] = X_train[num_cols].replace(-1, np.nan)
 X_test[num_cols] = X_test[num_cols].replace(-1, np.nan)
 
 # %%
-# 글자 데이터만 원핫 인코딩 변환 
-cat_cols = [
-    'brand', 'model_family','cpu_brand','cpu_family',
-    'cpu_generation','cpu_suffix','os'
-]
+# 4. 문자열 데이터 OneHotEncoding
+cat_cols = X.select_dtypes(include="str").columns.tolist()
 oh_encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
 
 X_train_cat = oh_encoder.fit_transform(X_train[cat_cols])
 X_test_cat = oh_encoder.transform(X_test[cat_cols])
 
 # %%
-# 4. 수치형 데이터와 원핫 인코딩된 데이터 합치기
+# 5. 수치형 데이터와 원핫 인코딩된 데이터 합치기
 X_train_final = np.hstack([X_train[num_cols].values, X_train_cat])
 X_test_final = np.hstack([X_test[num_cols].values, X_test_cat])
 
 # %%
-# 모델 학습 및 예측 
+# 6. 모델 학습 및 예측 
 xgb = XGBRegressor(random_state=42, n_estimators=100, learning_rate=0.1)
 xgb.fit(X_train_final, y_train)
 
@@ -72,33 +60,33 @@ train_preds = xgb.predict(X_train_final)
 test_preds = xgb.predict(X_test_final)
 
 # %%
-# 성능 지표 계산 및 출력
+# 7. 성능 지표 계산 및 출력
 mae = mean_absolute_error(y_test, test_preds)
 mse = mean_squared_error(y_test, test_preds)
 rmse = np.sqrt(mean_squared_error(y_test, test_preds))
 r2_train = r2_score(y_train,train_preds)
 r2_test = r2_score(y_test, test_preds)
 
-print("===== RandomForest =====")
-print(f"RF_MAE: {mae}")
-print(f"RF_MSE: {mse}")
-print(f"RF_RMSE: {rmse}")
-print(f"RF_R2: {r2_test:.3f}")
-print(f'RF_R2_train: {r2_train:.3f}')
-print(f'R2_Gap :{r2_train-r2_test:.3f}')
+print("\n===== XGBoost =====")
+print(f"XGB_MAE: {mae}")
+print(f"XGB_MSE: {mse}")
+print(f"XGB_RMSE: {rmse}")
+print(f"XGB_R2: {r2_test:.3f}")
+print(f'XGB_R2_train: {r2_train:.3f}')
+print(f'XGB_Gap :{r2_train-r2_test:.3f}')
 
 # %%
-# 가격 범위 계산에 쓸 residual 표준편차
+# 8. 가격 범위 계산에 쓸 residual 표준편차
 residuals = y_test-test_preds
 residual_std = np.std(residuals)
-print(f"residual_std: {residual_std}")
+print(f"\nresidual_std: {residual_std}")
 
 # %%
-# predict.py에서 인코딩을 똑같이 맞추기 위해 학습 때 쓴 컬럼 목록도 저장
-feature_columns = X_train.columns.tolist()
+# 9. predict.py에서 인코딩을 똑같이 맞추기 위해 학습 때 쓴 컬럼 목록도 저장
+feature_columns = oh_encoder.get_feature_names_out(cat_cols).tolist()
 
 # %%
-# 모델 저장
+# 10. 모델 저장
 # 현재 폴더에 models 폴더가 없으면 만듬
 os.makedirs("models", exist_ok=True)
 
@@ -110,10 +98,5 @@ joblib.dump(
     "residual_std" : residual_std,
 },"models/price_model_laptop.pkl")
 
-print('저장완료 : models/onehot_encoder.pkl')
+print('\n저장완료 : models/onehot_encoder.pkl')
 print("저장완료 : models/price_model_laptop.pkl")
-
-# %%
-print(os.getcwd())
-
-
